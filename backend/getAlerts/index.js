8f6f8a91a3a12665ef027e0fb49a1bea10899496
Dusko Sretenovic <acc.dsretenovic@gmail.com>
2025-04-21 14:31:34 +0200

    write getAlerts

--- backend/getAlerts/index.js
+++ backend/getAlerts/index.js
@@ -1,13 +1,7 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+// getAlerts/index.js
+const { listAlerts } = require("../shared");
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+module.exports = async function (context) {
+  const alerts = await listAlerts();
+  context.res = { body: alerts };
+};
